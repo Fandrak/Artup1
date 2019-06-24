81dda31faa0bce414f6d1a9f5256797128081ba6
fandrak <fanda.vondrak@gmail.com>
2019-06-24 16:05:20 +0200

    about page added

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const Index = () => (
+   <section>
+        <h1>Hello Wolrd from NEXT JS</h1>
+        <Link href="/about">
+            <a title="About NextJS">About This Project</a>
+        </Link>
+    </section> 
+)
+
+export default Index;
+ 
