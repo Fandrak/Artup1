cfe90150e4792509a18712cbc153c2d04cc658ab
fandrak <fanda.vondrak@gmail.com>
2019-06-25 19:35:43 +0200

    Next JS config

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
+import MyDocument from "next/document";
+import "../styles.scss";
+
 const Index = () => (
-   <section>
-        <h1>Hello Wolrd from NEXT JS</h1>
-        <Link href="/about">
-            <a title="About NextJS">About This Project</a>
-        </Link>
-    </section> 
-)
+  //    <section>
+  //         <h1>Hello Wolrd from NEXT JS</h1>
+  //         <Link href="/about">
+  //             <a title="About NextJS">About This Project</a>
+  //         </Link>
+  //     </section>
+  <>
+    <div className="header">
+      <h1>ARTup</h1>
+      <p>bring your ART up!</p>
+    </div>
+    <div className="nav">
+      <div className="container">
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Employees</li>
+        </ul>
+      </div>
+    </div>
+    <p className="about">ARTup is a ....</p>
+  </>
+);
 
 export default Index;
- 
